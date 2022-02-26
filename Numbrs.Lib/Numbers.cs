80d9e030ed87a998214421456eded2c54f59d2d5
Pavel Popov <legeon48@mail.ru>
2022-02-26 16:06:38 +0300

    Добавьте файлы проекта.

---
+++ Numbrs.Lib/Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Numbers.Lib
+{
+    public static class Numbers
+    {
+        private static bool IsSimple(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static void PrimeNum(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (IsSimple(i))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{i}, ");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public static void Fibonachi(int n)
+        {
+            uint[] temp = new uint[] { 1, 1 };
+
+            for (int i = 2; i <= n; i++)
+            {
+                temp[i % 2] = temp[0] + temp[1];
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{temp[i % 2]}  ");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ TestThreads.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Numbers.Lib;
+
+namespace TestThreads.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапозон простых чисел");
+
+            Console.Write("Начало диапазона: ");
+            int _start = int.Parse(Console.ReadLine());
+
+            Console.Write("Конец диапазона: ");
+            int _end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите N чисел Фибоначчи:");
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Thread threadPrimeNum = new Thread(() => Numbers.Lib.Numbers.PrimeNum(_start, _end));
+            threadPrimeNum.Start();
+
+            Thread threadFibonachi = new Thread(() => Numbers.Lib.Numbers.Fibonachi(n));           
+            threadFibonachi.Start();
+           
+            
+        }
+    }
+}
